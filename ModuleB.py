7812d9a2a622705f798cbda463c26fc004e2264b
hyeonx3 <vhyeonv2@gmail.com>
2024-11-29 16:45:23 +0900

    Update ModuleB.py
    edited title

--- ModuleB.py
+++ ModuleB.py
@@ -6,7 +6,7 @@ def visualize(d1,d2,subject):
     plt.hist(d2.iloc[0],bins=d2.iloc[0],label="여학생",alpha=0.5,edgecolor="pink")
     plt.xlabel("표준점수")
     plt.ylabel("학생 수")
-    plt.title(f"2024 수능 {subject} 과목 학생 성별 별 점수 분포()",size=18)
+    plt.title(f"2024 수능 {subject} 과목 학생 성별 별 점수 분포",size=18)
     plt.legend()
     plt.grid(axis='y')
 
