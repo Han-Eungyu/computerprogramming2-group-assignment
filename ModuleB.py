0e153ccfb94e3a659f4cc3cfe98e69b9e84ade0f
hyeonx3 <vhyeonv2@gmail.com>
2024-11-29 16:33:45 +0900

    Module B.2
    시각화 함수 visualize(남자 데이터,여자 데이터, 과목명)

---
+++ ModuleB.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def visualize(d1,d2,subject):
+    plt.figure(figsize=(11,7))
+    plt.hist(d1[0],bins=d1[0],label="남학생",alpha=0.5,edgecolor="blue")
+    plt.hist(d2[0],bins=d2[0],label="여학생",alpha=0.5,edgecolor="pink")
+    plt.xlabel("표준점수")
+    plt.ylabel("학생 수")
+    plt.title(f"2024 수능 {subject} 과목 학생 성별 별 점수 분포()",size=18)
+    plt.legend()
+    plt.grid(axis='y')
+
+    plt.show()
+
+
+
+
