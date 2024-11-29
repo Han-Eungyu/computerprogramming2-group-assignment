cb80d642cb6f639005939e6c59aae57e60587498
Han-Eungyu <eungyu8632@gmail.com>
2024-11-29 16:36:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import csv
 import data_proccessing as dp
 import ModuleB as mb
 
-subjects = dp.valid_subjects()
+file_path = "20231231.csv"
+file_encoding = "EUC-KR"
+if data is None:
+    print("데이터를 로드하지 못했습니다. 프로그램을 종료합니다.")
+    exit()
+    
+subjects = dp.valid_subjects(data)
 for i in subjects:
     print(i)
-
 
 while True:
     input_subject = input("과목 이름을 입력하세요 >> ")
