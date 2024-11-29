d531d1ac7bdabd194606bb189a6663af089ea3e7
Han-Eungyu <eungyu8632@gmail.com>
2024-11-29 16:14:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # main.py
 
+import csv
+import data_proccessing as dp
+
+for i in valid_subjects:
+    print(i)
+
+while True:
+    input_subject = input("과목 이름을 입력하세요 >> ")
+    if input_subject not in subjects:
+        print("잘못된 과목 이름입니다.")
+    else:
+        break
+print(f"{input_subject} 과목의 도수분포그래프입니다.")
