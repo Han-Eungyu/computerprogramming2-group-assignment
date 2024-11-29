759aace7ed7601045354135d2601fa6baf6fd98f
hyeonx3 <vhyeonv2@gmail.com>
2024-11-29 17:18:33 +0900

    edit ModuleB
    font edited

--- ModuleB.py
+++ ModuleB.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def visualize(d1,d2,subject):
+    plt.rcParams["font.family"] = "AppleGothic"
+    plt.rcParams["axes.unicode_minus"] = False
     plt.figure(figsize=(11,7))
     plt.bar(d1.iloc[:,0], d1.iloc[:,1], width=1.0, label="남성", alpha=0.5,color="skyblue") 
     plt.bar(d2.iloc[:,0], d2.iloc[:,1], width=1.0, label="여성", alpha=0.5,color="pink") 
@@ -11,9 +13,7 @@ def visualize(d1,d2,subject):
     plt.title(f"2024 수능 {subject} 과목 학생 성별 별 점수 분포",size=18)
     plt.legend()
     plt.grid(axis='y')
-    plt.rcParams['font.family'] = 'NanumGothic'
-
-
+    
     plt.show()
 
 
