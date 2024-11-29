b2b858a8099ebbebcdb3d0ec3df8ca402145c1d9
hyeonx3 <vhyeonv2@gmail.com>
2024-11-29 16:37:29 +0900

    ModuleB.3
    edited

--- ModuleB.py
+++ ModuleB.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 def visualize(d1,d2,subject):
     plt.figure(figsize=(11,7))
-    plt.hist(d1[0],bins=d1[0],label="남학생",alpha=0.5,edgecolor="blue")
-    plt.hist(d2[0],bins=d2[0],label="여학생",alpha=0.5,edgecolor="pink")
+    plt.hist(d1.iloc[0],bins=d1.iloc[0],label="남학생",alpha=0.5,edgecolor="blue")
+    plt.hist(d2.iloc[0],bins=d2.iloc[0],label="여학생",alpha=0.5,edgecolor="pink")
     plt.xlabel("표준점수")
     plt.ylabel("학생 수")
     plt.title(f"2024 수능 {subject} 과목 학생 성별 별 점수 분포()",size=18)
