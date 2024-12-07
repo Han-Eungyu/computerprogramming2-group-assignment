c0ef2f1a3591b433a19c624e18c1f4eee2c3a5d4
Han-Eungyu <eungyu8632@gmail.com>
2024-12-07 19:10:20 +0900

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,46 +1,30 @@
-# main.py
+import pandas as pd
 
-import csv
-import data_processing as dp
-import ModuleB as mb
+def load_data(file_path, file_encoding="EUC-KR"):
+    """데이터를 로드하는 함수"""
+    try:
+        data = pd.read_csv(file_path, encoding=file_encoding) 
+        return data
+    except Exception as e:
+        print(f"데이터 로드 중 오류: {e}")
+        return None
 
-while True:
-    year = input("연도를 입력하세요(2020 - 2023) >> ")
-    if ((year == "2020") or (year == "2021") or (year == "2022") or (year == "2023")):
-        break
+def valid_subjects(data):
+    """
+    데이터프레임에서 '유형' 열의 고유 값을 추출하는 함수.
+    """
+    return data["유형"].unique()
 
-if year == "2020":
-    file_path = "20201231.csv"
-elif year == "2021":
-    file_path = "20211231.csv"
-elif year == "2022":
-    file_path = "20221231.csv"
-else:
-    file_path = "20231231.csv"
+def filter_data_by_subject(data, subject):
+    """특정 과목 데이터만 필터링하는 함수"""
+    return data[data["유형"] == subject]
 
-file_encoding = "EUC-KR"
-
-data = dp.load_data(file_path, file_encoding)
-if data is None:
-    print("데이터를 로드하지 못했습니다. 프로그램을 종료합니다.")
-    exit()
-
-subjects = dp.valid_subjects(data)
-for i in subjects:
-    print(i)
-
-while True:
-    input_subject = input("과목 이름을 입력하세요 >> ")
-    if input_subject not in subjects:
-        print("잘못된 과목 이름입니다.")
-    else:
-        break
-print(f"{input_subject} 과목의 도수분포그래프입니다.")
-
-filtered_data = dp.filter_data_by_subject(data, input_subject)
-
-# 남학생과 여학생의 점수 분포 데이터 준비
-male_data, female_data = dp.prepare_distribution_data(filtered_data)
+def prepare_distribution_data(filtered_data):
+    """남학생과 여학생의 점수 분포 데이터를 준비하는 함수"""
+    male_data = filtered_data[["표준점수", "남자"]]
+    female_data = filtered_data[["표준점수", "여자"]]
+    
+    return male_data, female_data
 
 # 분포 그래프 시각화
 mb.visualize(male_data, female_data, input_subject)
